0e59c0afcbcc16bedfc5e682ae57a8ce77cb998e
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-04-29 15:51:13 +0300

    build: Пустой проект на Spring

---
+++ src/main/java/ru/zalimannard/wordboxbackend/WordBoxBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.zalimannard.wordboxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordBoxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordBoxBackendApplication.class, args);
+    }
+
+}
